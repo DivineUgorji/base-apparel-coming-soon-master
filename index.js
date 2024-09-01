c16c965b99f1f012c8ff28ebd42700241cd31594
Divine Ugorji <divineugorji37@gmail.com>
2024-09-01 09:55:52 +0100

    fininshing effects

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
+const emailSuccess = document.getElementById("email-success-page");
+const mainPage = document.getElementById("main-page");
+const submit = document.getElementById("submit");
+const formControl = document.querySelector(".form-control");
 
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -8,32 +12,37 @@ function showError(input, message) {
   const small = formControl.querySelector("small");
   small.innerText = message;
   errorIcon.classList.remove("hide-visibility");
+  formControl.classList.add("shake");
 }
+
+formControl.addEventListener("animationend", (e) => {
+  formControl.classList.remove("shake");
+});
 
 // show success message
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
+  emailSuccess.classList.remove("page-disabled");
+  mainPage.classList.add("page-disabled");
 }
 
-// function checkRequiredInput(emailInput) {}
-
-function isValidEmail(email) {
+function checkEmail(input) {
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailRegex.test(String(email).toLowerCase);
+  if (emailRegex.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Please provide a valid email");
+  }
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  // checkRequiredInput(email);
-
   if (email.value.trim() === "") {
     showError(email, "Please provide your email");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Please provide a valid email");
-  } else if (isValidEmail) {
-    showSuccess(email);
+  } else {
+    checkEmail(email);
   }
 });
